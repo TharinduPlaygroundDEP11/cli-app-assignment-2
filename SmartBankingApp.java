f7d49279081045b9af36759ac339ba29c70cf17c
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-08-16 20:00:31 +0530

    Initial Commit

---
+++ SmartBankingApp.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SmartBankingApp {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
